a31a3937b2a464f0ceadd9c03b51a5c316dc0549
USER PC2 <aleksandarisov0@gmail.com>
2024-02-04 17:09:15 +0300

    c

---
+++ hand_tests.py
@@ -0,0 +1,12 @@
+from functions import salary,hello_who
+
+if salary!= 20:
+    print('Error')
+if hello_who('Max') != 'Hello, Max':
+    print('Failed')
+else:
+    print('Good')
+if hello_who('Leo') != 'Hello,Leo':
+    print('Failed')
+else:
+    print('Amazing')
